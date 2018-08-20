63b21bd4aa3425d389f0301bdb15a6af231aca13
N8ers <nathanraysheryak@gmail.com>
2018-08-20 10:53:41 -0400

    usurvey component skeleton, beta tested

---
+++ u-survey/src/Usurvey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Usurvey extends Component {
+    render() {
+        return(
+            <div>
+                I am from Usurvey Component
+            </div>
+        );
+    }
+}
+
+export default Usurvey;

51fba6c9738cfa5c6f7f43bcde1f27ca3c8b02b5
N8ers <nathanraysheryak@gmail.com>
2018-08-21 07:42:06 -0400

    firebase added & basic form completed

--- u-survey/src/Usurvey.js
+++ u-survey/src/Usurvey.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+var firebase = require('firebase');
+var uuid = require('uuid');
+
+var config = {
+    apiKey: "AIzaSyCkx6jKR_qJiM-Cvfk6JNm13c3IoJUNOQ8",
+    authDomain: "usurvey-b49e7.firebaseapp.com",
+    databaseURL: "https://usurvey-b49e7.firebaseio.com",
+    projectId: "usurvey-b49e7",
+    storageBucket: "usurvey-b49e7.appspot.com",
+    messagingSenderId: "633894929479"
+  };
+  firebase.initializeApp(config);
 
 class Usurvey extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            uid: uuid.v1(),
+            studentName: '',
+            answers: {
+                answer1: '',
+                answer2: '',
+                answer3: ''
+            },
+            isSubmitted: false
+        };
+    }
+
     render() {
+        var studentName;
+        var questions;
+
+        if(this.state.studentName === '' && this.state.isSubmitted === false) {
+            studentName = <div>
+                <h1>Hey student, please let us know your name</h1>
+                <form>
+                    <input type="text" placeholder="john/jane" ref="name" />
+                </form>
+            </div>
+        }
+
         return(
             <div>
-                I am from Usurvey Component
+                {studentName}
+                --------------------------------------------
+                {questions}
             </div>
         );
     }
